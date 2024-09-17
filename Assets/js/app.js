ad459209c3cf932d4ada3f658b60504c51011bf3
Sebas <sebaslg96@gmail.com>
2024-09-17 00:36:10 -0500

    se agrego modal de mas info para pokemones, se agrego mas info al json y se añadieron mas funciones al js

--- Assets/js/app.js
+++ Assets/js/app.js
@@ -3,6 +3,7 @@ const txtBuscarPokemon = document.getElementById('txtBuscarPokemon')
 const dataZone = document.getElementById('dataZone')
 const btnPokemonRandom = document.getElementById('btnPokemonRandom')
 const displayMessage = document.getElementById('displayMessage')
+const modalPokemonContent = document.getElementById('modalPokemonContent')
 let randomActivated = false
 
 btnBuscarPokemon.addEventListener('click', ()=> {
@@ -11,7 +12,8 @@ btnBuscarPokemon.addEventListener('click', ()=> {
         randomActivated = false
     }
     let valor = txtBuscarPokemon.value
-    buscarPokemon(valor)
+    //buscarPokemon(valor)
+    getPokemon(valor)
     mensaje('Desplegando resultados')
 })
 
@@ -26,10 +28,229 @@ window.addEventListener('keydown', (e)=>{
             randomActivated = false
         }
         let valor = txtBuscarPokemon.value
-        buscarPokemon(valor)
+        //buscarPokemon(valor)
+        getPokemon(valor)
         mensaje('Desplegando resultados')
     }
 })
+
+// -------------- Funciones de cartas especificas
+function getPokemon(valor){
+    ruta = `https://pokeapi.co/api/v2/pokemon/${valor}/`
+
+    fetch(ruta)
+    .then(response => response.json())
+    .then(json => printPokemon(json))
+    .catch(err => console.log(err)); 
+}
+
+function printPokemon(data){
+    //console.log(data)
+    let tipos = ""
+
+
+    for(let element in data.types){
+        tipos += `<span class="pkm-type ${data.types[element].type.name}"> <b>${capitalizeFirstLetter(replacer(data.types[element].type.name))}</b></span>`
+    }
+
+    dataZone.innerHTML += `
+       <!-- <div class="row"> -->
+            <div class="col s3">
+                <div class="card">
+                    <div class="card-image waves-effect waves-block waves-light">
+                      <img class="activator" src="${data['sprites']['other']['official-artwork']['front_default']}">
+                    </div>
+                    <div class="card-content">
+                      <span class="card-title activator grey-text text-darken-4">${capitalizeFirstLetter(data.name)}<i class="material-icons right">expand_more</i></span>
+                    </div>
+                    <div class="card-reveal">
+                      <span class="card-title grey-text text-darken-4">${capitalizeFirstLetter(data.name)}<i class="material-icons right">close</i></span>
+                      <p>Número: ${data.id}</p>
+                      <p>Típos:</p>
+                      <p id="type${data.id}">
+                      ${tipos}
+                      </p>
+                      <p>Altura: ${data.height}</p>
+                      <p>Peso: ${data.weight}</p>
+                      <p><button onclick="setModalPokemon(${data.id})" data-target="modalPokemon" class="btn modal-trigger">Mas información</button></p>
+                    </div>
+                </div>
+            </div>
+        <!--</div>-->
+    `
+}
+
+function setModalPokemon(id){
+    ruta = `https://pokeapi.co/api/v2/pokemon/${id}/`
+
+    fetch(ruta)
+    .then(response => response.json())
+    .then(json => printModalPokemon(json))
+    .catch(err => console.log(err)); 
+}
+
+function printModalPokemon(data){
+    let tipos = ""
+    let habilidades = ""
+    let movimientos = ""
+    let deFrente = ""
+    let atras = ""
+    let deFrenteShiny = ""
+    let atrasShiny = ""
+    let esMacho = ""
+
+    let deFrenteFemenino = ""
+    let atrasFemenino = ""
+    let deFrenteShinyFemenino = ""
+    let atrasShinyFemenino = ""
+    let sonidos = ""
+
+    for(let element in data.types){
+        tipos += `<span class="pkm-type ${data.types[element].type.name}"> <b>${capitalizeFirstLetter(replacer(data.types[element].type.name))}</b></span>`
+    }
+
+    for(let element in data.abilities){
+        habilidades += `<span class="pkm-type">${data.abilities[element].ability.name} </span>`
+    }
+
+    for(let element in data.moves){
+        if (element >= data.moves.length-1) {
+            console.log(element+" "+data.moves.length)
+            movimientos += `<span>${data.moves[element].move.name} </span>`
+        }else{
+            movimientos += `<span>${data.moves[element].move.name} </span>,`
+        }
+        
+    }
+
+            //femeninos
+            if (data.sprites.front_female != null) {
+                esMacho = "(Macho)"
+                deFrenteFemenino = `
+                <p>De frente (Hembra)</p>
+                <img class="activator" src="${data.sprites.front_female}">
+            `
+            }
+        
+            if (data.sprites.back_female != null) {
+                esMacho = "(Macho)"
+                atrasFemenino = `
+                <p>Atrás (Hembra)</p>
+                <img class="activator" src="${data.sprites.back_female}">
+            `
+            }
+        
+            if (data.sprites.front_shiny_female != null) {
+                esMacho = "(Macho)"
+                deFrenteShinyFemenino = `
+                <p>De frente shiny (hembra)</p>
+                <img class="activator" src="${data.sprites.front_shiny_female}">
+            `
+            }
+        
+            if (data.sprites.back_shiny_female != null) {
+                esMacho = "(Macho)"
+                atrasShinyFemenino = `
+                <p>Atrás shiny (hembra)</p>
+                <img class="activator" src="${data.sprites.back_shiny_female}">
+            `
+            }
+    
+            //machos
+    
+        if (data.sprites.front_default != null) {
+            deFrente = `
+                <p>De frente ${esMacho}</p>
+                <img class="activator" src="${data.sprites.front_default}">
+            `
+        }
+    
+        if (data.sprites.back_default != null) {
+            atras = `
+                <p>Atrás ${esMacho}</p>
+                <img class="activator" src="${data.sprites.back_default}">
+            `
+        }
+    
+        if (data.sprites.front_shiny != null) {
+            deFrenteShiny = `
+                <p>De frente shiny ${esMacho}</p>
+                <img class="activator" src="${data.sprites.front_shiny}">
+            `
+        }
+    
+        if (data.sprites.back_shiny != null) {
+            atrasShinyFemenino = `
+                <p>Atrás shiny ${esMacho}</p>
+                <img class="activator" src="${data.sprites.back_shiny}">
+            `
+        }
+
+        if (data.cries.latest != null) {
+            if (data.cries.legacy != null) {
+                sonidos = `
+                    <p><b>Sonidos</b></p>
+                     <p>Actualizado</p>
+                    <audio controls>
+                        <source src="${data.cries.latest}" type="audio/ogg">
+                        Tu navegador no soporta este elemento
+                    </audio>
+                    <p>Traducional</p>
+                    <audio controls>
+                        <source src="${data.cries.legacy}" type="audio/ogg">
+                        Tu navegador no soporta este elemento
+                    </audio>
+                `
+            }else{
+                sonidos = `
+                    <p><b>Sonido</b></p>
+                    <audio controls>
+                        <source src="${data.cries.latest}" type="audio/ogg">
+                        Tu navegador no soporta este elemento
+                    </audio>
+                `
+            }
+        }
+
+    modalPokemonContent.innerHTML = 
+    `
+    <h4>${capitalizeFirstLetter(data.name)}</h4>
+    <div class="row">
+        <div class="col s7">
+            <img class="modal-pkm-image" src="${data['sprites']['other']['official-artwork']['front_default']}">
+        </div>
+        <div class="col s5">
+            <p><b>Típos</b></p>
+            <p id="type${data.id}">
+            ${tipos}
+            </p>
+            <p><b>Habilidades</b></p>
+            <p>${habilidades}</p>
+            <p>Altura: ${data.height}</p>
+            <p>Peso: ${data.weight}</p>
+            <p>
+            ${sonidos}
+            </p>
+        </div>
+    </div>
+
+
+
+    <p><b>Imagenes</b></p>
+    ${deFrente}
+    ${atras}
+    ${deFrenteShiny}
+    ${atrasShiny}
+    ${deFrenteFemenino}
+    ${atrasFemenino}
+    ${deFrenteShinyFemenino}
+    ${atrasShinyFemenino}
+    <p><b>Movimientos</b></p>
+    ${movimientos}
+    `
+}
+
+// -------------- FIN Funciones de cartas especificas
 
 function buscarPokemon(valor) {
 
@@ -181,7 +402,8 @@ function printCards(cantidad = null){
     
     for (let i = 0; i < cantidadCartas; i++) {
         let valor = random(1, 1025)
-        buscarPokemon(valor)
+        //buscarPokemon(valor)
+        getPokemon(valor)
     }
 }
 
@@ -270,6 +492,11 @@ function replacer(text){
     return respuesta
 }
 
+//modal
+document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.modal');
+    var instances = M.Modal.init(elems);
+});
 
 //getting the names
 
